ae94f5a3b4b627e881bc05d89e861b1cbe09fab9
huitoutunao <lh_huitoutunao@163.com>
2024-07-04 23:14:05 +0800

    feat: 新增变量

---
+++ basic/02_变量/index.py
@@ -0,0 +1,14 @@
+# 定义钱包金额
+money = 100
+
+print("钱包剩余：", money)
+
+# 买了一个冰淇淋，花了 10 元
+money = money - 10
+
+print("买了冰淇淋后钱包剩余：", money)
+
+# 买了瓶可乐，花了 5 元
+money = money - 5
+
+print("买了可乐后钱包剩余：", money)
