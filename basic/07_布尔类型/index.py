78d5cadd3ebec8f5a571bff529dcfc6416d40ea9
huitoutunao <lh_huitoutunao@163.com>
2024-07-08 23:26:14 +0800

    feat: 新增布尔类型

---
+++ basic/07_布尔类型/index.py
@@ -0,0 +1,19 @@
+# 定义变量存储布尔类型的数据
+bool_1 = True
+bool_2 = False
+print(f"bool_1变量的内容是：{bool_1}，类型是：{type(bool_1)}")
+print(f"bool_1变量的内容是：{bool_2}，类型是：{type(bool_2)}")
+
+# 比较运算符的使用
+# == != >= <= > <
+num1 = 10
+num2 = 10
+print(f"10 == 10 结果是：{num1 == num2}")
+
+num1 = 10
+num2 = 15
+print(f"10 != 10 结果是：{num1 != num2}")
+
+name1 = 'itcast'
+name2 = 'itethem'
+print(f"itcast == itethem 结果是：{name1 == name2}")
