b2a8559aca69341c3bbda8210d8098d93af8cc0f
huitoutunao <lh_huitoutunao@163.com>
2024-07-09 23:04:20 +0800

    feat: 新增布尔类型35%

--- basic/07_布尔类型/index.py
+++ basic/07_布尔类型/index.py
@@ -33,3 +33,12 @@ if age > 18:
     print("您已成年，游玩需要补票10元")
 
 print("祝你游玩愉快")
+
+# if else 语句
+age = int(input("请输入你的年龄："))
+if age >= 18:
+    print("您已成年，游玩需要补票10元")
+elif age >= 30:
+    print("else if")
+else:
+    print("未成年免费")
